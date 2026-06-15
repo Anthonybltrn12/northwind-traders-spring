b0c0da89e6acff25c27124668879dd026a4df99f
antbeltran <anthonybltrn@gmail.com>
2026-06-15 13:31:56 -0500

    Initial commit

---
+++ src/test/java/com/yearup/northwind_traders/NorthwindTradersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yearup.northwind_traders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
